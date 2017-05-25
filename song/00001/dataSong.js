ec34becada564f45f7c892b6b9528c9df95817fd
novguilherme <guilherme.nascimento@novacia.com.br>
2017-05-25 19:04:04 -0300

    update 2017-05-25

---
+++ song/00001/dataSong.js
@@ -0,0 +1,542 @@
+/* Time Buttons - Start */
+addMusicNote({
+	btn:"5",
+	time:"4"
+});
+
+addMusicNote({
+	btn:"5",
+	time:"7"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"9"
+});
+
+addMusicNote({
+	btn:"1",
+	time:"10"
+});
+
+addMusicNote({
+	btn:"2",
+	time:"11"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"12"
+});
+
+addMusicNote({
+	btn:"9",
+	time:"13"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"15"
+});
+
+addMusicNote({
+	btn:"5",
+	time:"15.5"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"16"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"17"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"17.5"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"19"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"20"
+});
+
+addMusicNote({
+	btn:"2",
+	time:"21"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"22"
+});
+
+addMusicNote({
+	btn:"5",
+	time:"23"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"24"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"25"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"26.5"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"27"
+});
+
+addMusicNote({
+	btn:"1",
+	time:"28"
+});
+
+addMusicNote({
+	btn:"2",
+	time:"29"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"30"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"31"
+});
+
+addMusicNote({
+	btn:"5",
+	time:"32"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"34"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"35"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"36"
+});
+
+addMusicNote({
+	btn:"9",
+	time:"36.5"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"38"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"39"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"40"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"41"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"43"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"44"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"44.5"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"45"
+});
+
+addMusicNote({
+	btn:"2",
+	time:"46"
+});
+
+addMusicNote({
+	btn:"3",
+	time:"47"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"48"
+});
+
+addMusicNote({
+	btn:"5",
+	time:"50"
+});
+
+addMusicNote({
+	btn:"6",
+	time:"51"
+});
+
+addMusicNote({
+	btn:"7",
+	time:"52"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"53"
+});
+
+addMusicNote({
+	btn:"9",
+	time:"54"
+});
+
+addMusicNote({
+	btn:"4",
+	time:"55"
+});
+
+addMusicNote({
+	btn:"8",
+	time:"56"
+});
+
+addMusicNote({
+	btn:"9",
+	time:"57"
+});
+
+addMusicNote({
+	btn:"3",
+	timeStr:"59"
+});
+
+addMusicNote({
+	btn:"7",
+	timeStr:"59.5"
+});
+
+addMusicNote({
+	btn:"6",
+	timeStr:"60"
+});
+
+addMusicNote({
+	btn:"5",
+	timeStr:"61"
+});
+
+addMusicNote({
+	btn:"5",
+	timeStr:"63"
+});
+
+addMusicNote({
+	btn:"8",
+	timeStr:"63.5"
+});
+
+addMusicNote({
+	btn:"9",
+	timeStr:"64"
+});
+
+addMusicNote({
+	btn:"7",
+	timeStr:"66"
+});
+/*
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+
+addMusicNote({
+	btn:"",
+	timeStr:""
+});
+//alert(musicNote[0].timeStr);
+/* Time Buttons - End */
